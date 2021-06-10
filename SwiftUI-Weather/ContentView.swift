9bff121f8d100cb692a5b63101df448b1b6bb9b6
John Schils <JohnASchils@gmail.com>
2021-06-09 23:18:17 -0400

    Added SF Symbol image in new VStack along with city and temp text, using Spacer() to fill remaining space

--- SwiftUI-Weather/ContentView.swift
+++ SwiftUI-Weather/ContentView.swift
@@ -14,6 +14,25 @@ struct ContentView: View {
                            startPoint: .topLeading,
                            endPoint: .bottomTrailing)
                 .edgesIgnoringSafeArea(.all)
+            VStack {
+                Text("Saline, MI")
+                    .font(.system(size: 32, weight: .medium, design: .default))
+                    .foregroundColor(.white)
+                    .padding()
+                VStack(spacing: 10) {
+                    Image(systemName: "cloud.sun.fill")
+                        .renderingMode(.original)
+                        .resizable()
+                        .aspectRatio(contentMode: .fit)
+                        .frame(width: 180, height: 180)
+                    Text("75°")
+                        .font(.system(size: 70, weight: .medium, design: .default))
+                        .foregroundColor(.white)
+                    
+                }
+                
+                Spacer() //fills remaining space
+            }
         }
     }
 }
